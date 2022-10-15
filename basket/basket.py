7b55759e6bfe85ea4a9c126414da62ecca2d7fa0
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-10-16 00:03:18 +0200

    Add basket summery page

--- basket/basket.py
+++ basket/basket.py
@@ -1,3 +1,8 @@
+from decimal import Decimal
+
+from store.models import Product
+
+
 class Basket:
     """
     A base Basket class, providing some default behaviors that
@@ -17,6 +22,23 @@ class Basket:
         """
         return sum(item['qty'] for item in self.basket.values())
 
+    def __iter__(self):
+        """
+        Collect the product_id in the session data to query the database
+        and return products
+        """
+        product_ids = self.basket.keys()
+        products = Product.products.filter(id__in=product_ids)
+        basket = self.basket.copy()
+
+        for product in products:
+            basket[str(product.id)]['product'] = product
+
+        for item in basket.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['qty']
+            yield item
+
     def add(self, product, qty):
         """
         Adding and updating the users basket session data
@@ -29,6 +51,9 @@ class Basket:
             self.basket[product_id] = {'price': str(product.price), 'qty': qty}
 
         self.save()
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())
 
     def save(self):
         self.session.modified = True
