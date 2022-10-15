6fd485c6e1d6f4f6954608a510087307731663fc
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-10-15 18:14:36 +0200

    Add show category details

--- store/urls.py
+++ store/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = 'store'
 
 urlpatterns = [
-    path('', views.all_products, name='all_products')
+    path('', views.all_products, name='all_products'),
+    path('<slug:slug>', views.product_detail, name='product_detail'),
+    path('shop/<slug:category_slug>/', views.category_list, name='category_list'),
+
 ]
