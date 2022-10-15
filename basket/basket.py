3b56a203884bee138ee9836a14f6f7f66aa19f6d
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-10-15 23:17:01 +0200

    Add basket ajax function

--- basket/basket.py
+++ basket/basket.py
@@ -1,4 +1,3 @@
-
 class Basket:
     """
     A base Basket class, providing some default behaviors that
@@ -11,3 +10,25 @@ class Basket:
         if 'skey' not in request.session:
             basket = self.session['skey'] = {}
         self.basket = basket
+
+    def __len__(self):
+        """
+        Get the basket data and count the qty of items
+        """
+        return sum(item['qty'] for item in self.basket.values())
+
+    def add(self, product, qty):
+        """
+        Adding and updating the users basket session data
+        """
+        product_id = str(product.id)
+
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] = qty
+        else:
+            self.basket[product_id] = {'price': str(product.price), 'qty': qty}
+
+        self.save()
+
+    def save(self):
+        self.session.modified = True
